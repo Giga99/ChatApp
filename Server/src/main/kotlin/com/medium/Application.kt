5058d919a31208d40568f8c4c08022e1159b7e7b
Giga99 <igorstevanovic99@gmail.com>
2022-09-14 18:24:56 +0200

    Added user and connected to the mongo db

--- Server/src/main/kotlin/com/medium/Application.kt
+++ Server/src/main/kotlin/com/medium/Application.kt
@@ -1,13 +1,27 @@
 package com.medium
 
-import io.ktor.server.application.*
+import com.medium.data.user.MongoUserDataSource
+import com.medium.data.user.User
 import com.medium.plugins.*
+import io.ktor.server.application.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    val mongoUsername = System.getenv("MONGO_USERNAME")
+    val mongoPassword = System.getenv("MONGO_PASSWORD")
+    val dbName = "chat-app"
+    val db = KMongo.createClient(
+        connectionString = "mongodb+srv://$mongoUsername:$mongoPassword@cluster0.kpks9jj.mongodb.net/$dbName?retryWrites=true&w=majority"
+    ).coroutine.getDatabase(dbName)
+    val userDataSource = MongoUserDataSource(db)
+
     configureHTTP()
     configureSecurity()
     configureMonitoring()
---
+++ Server/src/main/kotlin/com/medium/data/user/UserDataSource.kt
@@ -0,0 +1,8 @@
+package com.medium.data.user
+
+interface UserDataSource {
+
+    suspend fun getUserByUsername(username: String): User?
+
+    suspend fun insertUser(user: User): Boolean
+}
