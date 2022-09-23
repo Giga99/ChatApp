1367a90bb8900b893e1af999149c9019ffbdebf1
Giga99 <igorstevanovic99@gmail.com>
2022-09-23 03:14:51 +0200

    Server response refactor

--- Server/src/main/kotlin/com/medium/plugins/Serialization.kt
+++ Server/src/main/kotlin/com/medium/plugins/Serialization.kt
@@ -3,9 +3,19 @@ package com.medium.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                encodeDefaults = true
+                isLenient = true
+                allowSpecialFloatingPointValues = true
+                allowStructuredMapKeys = true
+                prettyPrint = true
+                useArrayPolymorphism = false
+            }
+        )
     }
 }
